fa807bb8391409e93beb2c959c917e089374a26a
dmpeterson317 <dmpeterson317@gmail.com>
2019-03-25 22:57:51 -0400

    v1

--- homework.js
+++ homework.js
@@ -1,26 +1,63 @@
 // 1. Set the variable `givenName` to the string "Addison".
+var givenName = "Addison"
 
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
+let candies = 20
+let people = 6
+let leftover = 20 % 6
 
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+// function greeting(name) {
+//     return 'Hello, ' + name + "!";
+// }
+function greeting(name) {
+    return (`Hello, ${name}!`)
+}
 
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
+// function isOdd(num) {
+//     if (num % 2 == 0) {
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+function isOdd(num) {
+    if (num % 2 === 1 || num % 2 === -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+function isEven(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
+function fahrenheitToCelsius(fTemp) {
+    return ((fTemp - 32) * 5 / 9);
+}
 
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+function celsiusToFahrenheit(cTemp) {
+    return ((cTemp * 9 / 5) + 32);
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -29,9 +66,21 @@
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
 
+function fahrenheitToKelvin(fTemp) {
+    var x = fahrenheitToCelsius(fTemp)
+    return (x + 273.15)
+}
+
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+function lesser(num1, num2) {
+    if (num1 < num2) {
+        return num1
+    } else {
+        return num2
+    }
+}
 
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -44,29 +93,84 @@
 // eo - Saluton, <name>!
 //
 // If any other language code is used, return nothing.
+function multigreeting(name, langCode) {
+    let helloTranslation;
+    if (langCode == 'en') {
+        helloTranslation = 'Hello';
+    } else if (langCode == 'es') {
+        helloTranslation = '¡Hola';
+    } else if (langCode == 'fr') {
+        helloTranslation = 'Bonjour';
+    } else if (langCode == 'eo') {
+        helloTranslation = 'Saluton';
+    } else {
+        return;
+    }
+    // return helloTranslation + ', ' + name + '!';
+    return `${helloTranslation}, ${name}!`;
+}
 
 // 10. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+function sum(numArray) {
+    let total = 0;
+    for (let num of numArray) {
+        total += num;
+    }
+    return total;
+}
 
 // 11. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+function average(numArray) {
+    if (numArray.length > 0) {
+        let total = sum(numArray);
+        return total / numArray.length;
+    } 
+    return;
+}
 
 // 12. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+function minimum(numArray) {
+    let currentMin = numArray[0];
+    for (let num of numArray) {
+        if (num < currentMin) {
+            currentMin = num;
+        }
+    }
+    return currentMin;
+}
 
 // 13. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
 // implement sorting ourselves, however.
 //
 // A "selection sort" is one of the most simple sorting algorithms. To implement it,
-// you start with an unsorted array of numbers. You search the array and find the
-// smallest number in the array. You then insert that into a new array as the first
-// element and remove it from the original array. You continue doing this until
-// there are no numbers left in the original array.
-//
+
+function selectionSort(unsortedArray) { 
+    var unsortedCopy = unsortedArray.slice(0);
+    let sortedArray = [];
+    for (let i = 0; i < unsortedArray.length; i++) {
+        let min = minimum(unsortedCopy);
+        unsortedCopy.splice(unsortedCopy.indexOf(min), 1)
+        sortedArray.splice(i, 0, min);
+    }
+    return sortedArray
+}
+
+// 1) start with an unsorted array of numbers. 
+
+// 2) search the array and find the smallest number in the array. 
+
+// 3) insert the smallest number into a new array as the first element and 
+// remove it from the original array. 
+
+// 4) continue doing this until there are no numbers left in the original array.
+
 // Create a function called selectionSort that takes an array of numbers and returns
 // a sorted array using the above technique.
-//
+
 // Note 1: You do not actually want to delete things from the original array. You
 // should copy it first. To copy an array, use the following code:
 //
